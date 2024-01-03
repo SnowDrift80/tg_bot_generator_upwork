42db5f8e13002dbf8e1b6d2ecb6f3ed149f336aa
SnowDrift80 <snowdrift80@yandex.com>
2024-01-03 19:06:06 +0100

    fixed issue with skipping chapter 4.

--- tgbot.py
+++ tgbot.py
@@ -132,7 +132,7 @@ async def create_scaffold(update: Update, context: ContextTypes.DEFAULT_TYPE, as
             continue
         line = line.lstrip()
         print(line)
-        if line.strip().startswith(('1.', '2.', '3.', '5.', '6.', '7.', '8.', '9.', 
+        if line.strip().startswith(('1.', '2.', '3.', '4.', '5.', '6.', '7.', '8.', '9.', 
                                    '10.', '11.', '12.', '13.', '14.', '15.', '16.', '17.', 
                                    '18.', '19.', '20.', '20.', '21.', '22.', '23.', '24.',
                                    '25.', '26.', '27.', '28.', '29.', '30.')):
