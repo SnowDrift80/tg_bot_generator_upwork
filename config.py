dcad73604259dc42aac14f973e8243610878033b
SnowDrift80 <snowdrift80@yandex.com>
2024-01-03 17:48:21 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+class BOT_CONSTANTS:
+    OPENAI_API_KEY = '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'
+    OPENAI_INSTRUCTIONS = 'Always answer in the same language in which you were asked a question.'
+    OPENAI_FILE_ID = 'file-b0w5vx0LtqwA3uv3jDhwSPOM'
+    OPENAI_ASSISTANT_ID = 'asst_8gBOLO4RVasmZzsrIxz9n0pv'
+    OPENAI_ERROR_MSG = 'The AI is not responsive. You may need to re-enter your request at a later time.'
+
+    TG_BOT_TOKEN = '6881817627:AAEgL44OZ_dklg7YmP872LUkDg-okKd3j8U'
+    TG_BOT_GREETING = 'I am an mRNA Expert Bot, ready to answer your questions regarding mRNA vaccines.'
+    TG_MAX_THREADS = 10000
+    TG_MAX_MESSAGES = 10000
+    TG_BOT_CALLSIGNS = [
+        "mRNAExpert",
+        "bohouš",
+        "bot",
+    ]
+    VERBOSE = True
